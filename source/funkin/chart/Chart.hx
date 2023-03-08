5a8fd9837f175ed45be923e6c116ebb2ae915c4d
FizzyBot <mranimationmaker27flip@gmail.com>
2023-03-08 15:05:32 -0500

    piss

---
+++ source/funkin/chart/Chart.hx
@@ -0,0 +1,10 @@
+package funkin.chart;
+
+import flixel.FlxG;
+import funkin.game.objects.HealthIcon;
+import funkin.scenes.PlayState;
+import funkin.system.Song.BPMChangeEvent;
+class Chart
+{
+    // This is the chart editor, will do this later or maybe when someone else will do it
+}
---
+++ source/funkin/system/Song.hx
@@ -0,0 +1,6 @@
+package funkin.system;
+
+class Song
+{
+
+}
