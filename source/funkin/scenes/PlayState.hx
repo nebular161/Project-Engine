9a9470cbb0708d4c1e6b2cb72cd940b1b83357f0
FizzyBot <mranimationmaker27flip@gmail.com>
2023-03-09 16:37:26 -0500

    mark a player

--- source/funkin/scenes/PlayState.hx
+++ source/funkin/scenes/PlayState.hx
@@ -1,5 +1,6 @@
 package funkin.scenes;
 
+import funkin.game.objects.HealthIcon;
 import flixel.FlxG;
 import flixel.FlxGame;
 import flixel.FlxSprite;
@@ -9,11 +10,21 @@ import flixel.system.FlxSound;
 import flixel.text.FlxText;
 import funkin.game.objects.StrumNote;
 import funkin.system.MusicBeatState;
+import funkin.system.FNFCamera;
 import haxe.Json;
 import lime.utils.Assets;
+#if discord_rpc
+import funkin.system.Discord.DiscordClient;
+#end
 
 class PlayState extends MusicBeatState
 {
+	/* Public Variables */
+	public var iconP1:HealthIcon;
+	public var iconP2:HealthIcon;
+
+	FlxG.mouse.visible = false;
+
 	override public function create()
 	{
 		super.create();
