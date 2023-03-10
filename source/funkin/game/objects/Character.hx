4b2f5a2031aa7063f9170853146cc86c079acf30
Joalor64 <rhgarcon@gmail.com>
2023-03-10 12:15:43 -0500

    make Boyfriend.hx a subclass

--- source/funkin/game/objects/Character.hx
+++ source/funkin/game/objects/Character.hx
@@ -1,3 +1,42 @@
 package funkin.game.objects;
 
-class Character {}
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.util.FlxTimer;
+
+using StringTools;
+
+class Character 
+{    
+}
+
+class Boyfriend extends Character
+{
+    public var stunned:Bool = false;
+    public function new(x:Float, y:Float, ?char:String = 'bf')
+    {
+        super(x, y, char, true);
+    }
+    
+    override function update(elapsed:Float)
+    {
+        if (!debugMode && animation.curAnim != null)
+        {
+            if (animation.curAnim.name.startsWith('sing'))
+            {
+                holdTimer += elapsed;
+            }
+            else
+                holdTimer = 0;
+    
+            if (animation.curAnim.name.endsWith('miss') && animation.curAnim.finished && !debugMode)
+            {
+                dance();
+            }
+    
+        }
+    
+        super.update(elapsed);
+    }
+}
