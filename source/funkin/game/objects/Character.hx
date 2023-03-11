416237198243abf7cb03ca7947e151fa863860c5
FizzyBot <mranimationmaker27flip@gmail.com>
2023-03-11 15:57:49 -0500

    .

--- source/funkin/game/objects/Character.hx
+++ source/funkin/game/objects/Character.hx
@@ -5,7 +5,21 @@ import flixel.FlxSprite;
 import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.util.FlxTimer;
 import funkin.system.Paths;
-
+import funkin.game.CoolUtil;
+import flixel.animation.FlxBaseAnimation;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.math.FlxPoint;
+import flixel.util.FlxColor;
+import haxe.Exception;
+import haxe.Json;
+import haxe.format.JsonParser;
+import haxe.xml.Access;
+import lime.utils.Assets;
+import openfl.utils.Assets as OpenFlAssets;
+#if sys
+import sys.FileSystem;
+import sys.io.File;
+#end
 using StringTools;
 
 class Character 
